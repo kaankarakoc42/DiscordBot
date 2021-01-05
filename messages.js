0c5dc846c2fcb7f134dc1cf31f82fd42444f91f1
kaan karakoç <56826739+kaankarakoc42@users.noreply.github.com>
2021-01-05 15:21:15 +0300

    Update messages.js

--- messages.js
+++ messages.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 
+const image="https://instagram.fayt2-1.fna.fbcdn.net/v/t51.2885-19/s150x150/120929233_378816783146613_4152190279759171459_n.jpg?_nc_ht=instagram.fayt2-1.fna.fbcdn.net&_nc_ohc=JWNa_wHaN3MAX_s9Psl&tp=1&oh=88504ec914fbb730cd3a14af1d9a8c87&oe=601CB3FF"
 function createMessage(config){
     var message = new Discord.MessageEmbed()
     if (typeof config.color !== 'undefined'){message.setColor(`${config.color}`)}
@@ -17,7 +18,7 @@ function createMessage(config){
 	if(typeof config.fieldTitle!=='undefined'){message.addField(`${config.fieldTitle}`, `${config.fieldin}`, true)}
 	if(typeof config.image!=='undefined'){message.setImage(`${config.image}`)}
 	message.setTimestamp()
-    if(typeof config.footer!=='undefined'){message.setFooter(`${config.footer.title}`,`${config.footer.image}`);}
+    message.setFooter("kaan karakoç",`${image}`);
     return message
 }
 module.exports=createMessage;
